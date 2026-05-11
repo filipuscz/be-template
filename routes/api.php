0c750e4b5034b71ecb89d4314a28add2fe57c0a2
filipuscz <filipus.chris@gmail.com>
2026-05-11 11:41:01 +0700

    refactor: simplify route definitions by importing controllers and remove redundant v2 API routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Helpers\ApiTokenHelper;
+use App\Http\Controllers\Api\v1\AuthController;
+use App\Http\Controllers\Api\v1\ExampleController;
 use App\Http\Middleware\CheckApiToken;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,7 +17,7 @@ Route::prefix('v1')
         Route::name('example.')
             ->prefix('example')
             ->middleware('auth:api')
-            ->controller(App\Http\Controllers\Api\v1\ExampleController::class)
+            ->controller(ExampleController::class)
             ->group(function () {
                 Route::get('/', 'index')->name('index');
                 Route::post('/', 'store')->name('store');
@@ -28,38 +29,12 @@ Route::prefix('v1')
             });
         Route::name('auth.')
             ->prefix('auth')
-            ->controller(App\Http\Controllers\Api\v1\AuthController::class)
+            ->controller(AuthController::class)
             ->group(function () {
                 Route::post('/login', 'login')->name('login')->middleware('guest')->withoutMiddleware(CheckApiToken::class);
                 Route::post('/logout', 'logout')->name('logout')->middleware('auth:api');
                 Route::post('/register', 'register')->name('register')->middleware('guest');
                 Route::get('/me', 'me')->name('me')->middleware('auth:api');
                 Route::get('/api-token', 'generateApiToken')->name('api-token')->middleware('auth:api')->withoutMiddleware(CheckApiToken::class);
-            });
-    });
-
-Route::prefix('v2')
-    ->name('api.v2.')
-    ->group(function () {
-        Route::name('example.')
-            ->prefix('example')
-            ->middleware('auth:api')
-            ->controller(App\Http\Controllers\Api\v1\ExampleController::class)
-            ->group(function () {
-                Route::get('/', 'index')->name('index');
-                Route::post('/', 'store')->name('store');
-                Route::get('/show/{idOrSlug}', 'show')->name('show');
-                Route::put('/update/{idOrSlug}', 'update')->name('update');
-                Route::delete('/delete/{idOrSlug}', 'destroy')->name('delete');
-                Route::put('/bulk_action/update', 'bulkUpdate')->name('bulk_action.update');
-                Route::match(['delete', 'post'], '/bulk_action/destroy', 'bulkDestroy')->name('bulk_action.destroy');
-            });
-        Route::name('auth.')
-            ->prefix('auth')
-            ->controller(App\Http\Controllers\Api\v1\AuthController::class)
-            ->group(function () {
-                Route::post('/login', 'login')->name('login')->middleware('guest');
-                Route::post('/logout', 'logout')->name('logout')->middleware('auth:api');
-                Route::get('/me', 'me')->name('me')->middleware('auth:api');
             });
     });
